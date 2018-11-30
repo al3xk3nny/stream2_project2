a5fd1e7d74a3ec9096fda95a01d95d9205a1a907
Alex Kenny <alexkenny1@gmail.com>
2018-11-30 15:37:24 +0000

    Added functionality for images

--- opportunities/models.py
+++ opportunities/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     type = models.CharField(max_length=10, default="marketer", null=False, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, related_name='posts', null=False, default=1, on_delete=models.SET_DEFAULT)
-    image = models.ImageField(upload_to="images", null=True, blank=True)
+    image = models.ImageField(upload_to="images", default="images/no_image.png", null=True, blank=True)
     
     def __str__(self):
         return self.title
@@ -24,7 +24,7 @@ class Message(models.Model):
     post = models.ForeignKey(Post, related_name='messages', null=False, default=1, on_delete=models.SET_DEFAULT)
     recipient = models.ForeignKey(User, related_name='messages_received', null=False, default=1, on_delete=models.SET_DEFAULT)
     author = models.ForeignKey(User, related_name='messages_sent', null=False, default=1, on_delete=models.SET_DEFAULT)
-    image = models.ImageField(upload_to="images", null=True, blank=True)
+    image = models.ImageField(upload_to="images", default="images/no_image.png", null=True, blank=True)
     
     def __str__(self):
         return self.title
